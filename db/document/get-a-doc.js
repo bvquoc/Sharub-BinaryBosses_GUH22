1c971c53b9d7f2283c38ea74a0e896d542d3602c
bvquoc <bvquoc.dev@gmail.com>
2022-11-27 01:16:25 +0700

    get doc API

---
+++ db/document/get-a-doc.js
@@ -0,0 +1,20 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { firestore } from '../../firebase/clientApp';
+
+/**
+ * Add a document to collection (req docId)
+ * @param {String} collectionName (ex: "products")
+ * @param {String} docId (ex: "12345" -> "products/12345")
+ */
+const getDocument = async (collectionName, docId) => {
+  const docRef = doc(firestore, collectionName, docId);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log('Document data:', docSnap.data());
+  } else {
+    console.log('No such document!');
+  }
+};
+
+export { getDocument };
