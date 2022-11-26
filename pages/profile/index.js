1397ba1dc2ffbdec69e3984b9edd53038020cf9c
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2022-11-26 21:30:22 +0700

    Edit logout

---
+++ pages/profile/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <div>Profile</div>
+    </>
+  );
+};
+
+export default Profile;
